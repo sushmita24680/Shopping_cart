6ce28f907f94564be54412d5060142b8200ef3ff
Sushmita Kumari <63455991+sushmita24680@users.noreply.github.com>
2022-04-19 15:51:05 +0530

    corrected cart index value

--- php/header.php
+++ php/header.php
@@ -26,7 +26,7 @@
                         <?php
 
                         if (isset($_SESSION['cart'])){
-                            $count = count($_SESSION['cart'])-1;
+                            $count = count($_SESSION['cart']);
                             echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
                         }else{
                             echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
